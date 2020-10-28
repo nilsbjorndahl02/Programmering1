07f4ad2e3dd95d861685bbdc18cf9298adc7b91b
nilsbjorndahl02 <nils.bjorndahl@elev.ga.ntig.se>
2020-10-28 09:21:21 +0100

    commit 3

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Hangman
 {
@@ -6,11 +7,71 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till Hangman")
+            Console.WriteLine("Välkommen till Hangman");
             Console.WriteLine("Skriv ditt namn");
-            person1._name = Console.ReadLine();
+            string namn = Console.ReadLine();
+            Console.WriteLine("hej " + namn);
             string[] ordlista = new string[3] { "hundleksak", "kiwi", "aprikos" };
 
+            Random random = new Random();
+
+            int slumpTal = random.Next(ordlista.Length);
+
+            string ord = ordlista[slumpTal];
+
+            char[] stars = new char[ord.Length];
+
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = '*';
+            }
+
+            int antalGissningar = 10;
+
+            do
+            {
+                Console.WriteLine("Gissa på ordet!");
+                string gissning = Console.ReadLine();
+
+                if (ord.Contains(gissning))
+                {
+                    //fyll på starWord
+                    char[] ordChars = ord.ToCharArray();
+
+                    for (int i = 0; i < ordChars.Length; i++)
+                    {
+                        if (gissning.ToCharArray()[0] == ordChars[i])
+                        {
+                            stars[i] = gissning.ToCharArray()[0];
+
+                        }
+                    }
+
+                    Console.WriteLine(stars);
+
+                    string starWord = new string(stars);
+
+                    if (starWord.Contains("*"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Winner Winner Chicken Dinner!");
+                        break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Du gissa fel!");
+                    antalGissningar--;
+                    Console.WriteLine("Du har: " + antalGissningar + " kvar!");
+
+                    //om personen gissar fel
+                }
+
+            } while (antalGissningar > 0);
 
         }
     }
